b49562608303f00414c816d4d8db25c08ca0a90f
abilcode <m.nabilfad@gmail.com>
2021-11-24 15:20:30 +0700

    passes dynamics data

--- react basics/src/App.js
+++ react basics/src/App.js
@@ -1,10 +1,29 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Title", amount: "Price", date: new Date(2021, 1, 1) },
+    { title: "Car", amount: 200, date: new Date(2021, 2, 28) },
+    { title: "Boat", amount: 2000, date: new Date(2021, 2, 18) },
+  ];
   return (
     <div>
       <h2>Let's get started, Guys!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- react basics/src/components/ExpenseItem.js
+++ react basics/src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = "Date";
-  const expenseTitle = "Title";
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div>{expenseDate}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className='expense-item__price'>
-        <h2>Price</h2>
+        <h2>{props.amount}</h2>
       </div>
     </div>
   );
