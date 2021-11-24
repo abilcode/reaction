4924393014790a1baeb4a694c0987c79ef6e9d1a
abilcode <m.nabilfad@gmail.com>
2021-11-24 13:59:01 +0700

    Outputting dynamic data

--- react basics/src/components/ExpenseItem.js
+++ react basics/src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = "Date";
+  const expenseTitle = "Title";
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{expenseDate}</div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
+        <h2>{expenseTitle}</h2>
       </div>
       <div className='expense-item__price'>
         <h2>Price</h2>
